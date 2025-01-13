a2791457667802e003997c43ab8d35ad0b00651e
sullivanlng <sullivanluong@gmail.com>
2025-01-13 21:39:35 +0100

    Update Email.js

--- spam-detector-backend/src/models/Email.js
+++ spam-detector-backend/src/models/Email.js
@@ -1,25 +1,29 @@
-// models/Email.js
+// Import de Mongoose pour gérer les interactions avec la base de données MongoDB
 const mongoose = require('mongoose');
 
+// Définition du schéma pour les emails analysés
 const emailSchema = new mongoose.Schema({
   object: {
-    type: String,
-    required: true,
+    type: String, // Le sujet de l'email
+    required: true, // Le sujet est obligatoire
   },
   body: {
-    type: String,
-    required: true,
+    type: String, // Le corps du message
+    required: true, // Le corps est obligatoire
   },
   result: {
-    type: String,
-    enum: ['spam', 'non-spam', 'phishing'],
-    required: true,
+    type: String, // Le résultat de l'analyse de l'email
+    enum: ['spam', 'non-spam', 'phishing'], // Limité aux valeurs possibles
+    required: true, // Le résultat est obligatoire
   },
   date: {
-    type: Date,
-    default: Date.now,
+    type: Date, // Date d'analyse de l'email
+    default: Date.now, // Par défaut, la date actuelle
   },
 });
 
+// Création du modèle Email basé sur le schéma défini
 const Email = mongoose.model('Email', emailSchema);
+
+// Export du modèle pour pouvoir l'utiliser dans d'autres fichiers
 module.exports = Email;
